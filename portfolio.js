13ed3ccb5e05b90bb01a6ef5a5cd5d6c0bae0cf1
Levi8853 <owoeyevictorght2019@gmail.com>
2025-10-21 12:43:45 +0100

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -23,7 +23,7 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-            // active section for aniumation on scroll
+            // active section for animation on scroll
             sec.classList.add('show-animate');
         }
         // if want to use animation that repeat on scroll use this
